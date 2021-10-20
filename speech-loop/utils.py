ccc9835bf74cb3241e26ab88df3e844348f6685f
Samuel Michalik <samuelmichalik@yahoo.com>
2021-10-20 18:04:30 +0200

    add utils module; text to crowns function

---
+++ speech-loop/utils.py
@@ -0,0 +1,11 @@
+CHARS_PER_TOK = 4
+CZK_PER_USD = 21.93
+USD_PER_1000_TOKS = 0.06 # for Davinci
+
+def text_to_crowns(text):
+    """Apply GPT-3 pricing on the text and return the approximate price in Czech crowns."""
+    tokens = len(text) / CHARS_PER_TOK
+    return tokens * USD_PER_1000_TOKS / 1000 * CZK_PER_USD
+
+def text_coda(text):
+    return f'{text}\nThis text cost {text_to_crowns(text)} crowns.'
