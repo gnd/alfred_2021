de492e5ea6e643f4c180dd92003d54778dc7494a
gnd <gnd@itchybit.org>
2022-01-18 22:46:42 +0000

    latest pin layout

--- pianobot/pianobot.py
+++ pianobot/pianobot.py
@@ -11,18 +11,18 @@ from osc4py3 import oscmethod as osm
 GPIO.setmode(GPIO.BOARD)
 
 # define solenoid 01 - solenoid 12 pins
-sol_01 = 16    # using 16 and 18 because of 1.8KO pullup
-sol_02 = 18
-sol_03 = 7
-sol_04 = 11
-sol_05 = 22
-sol_06 = 15
-sol_07 = 19
-sol_08 = 21
-sol_09 = 23
-sol_10 = 29
-sol_11 = 31
-sol_12 = 37
+sol_01 = 37
+sol_02 = 35
+sol_03 = 33
+sol_04 = 31
+sol_05 = 29
+sol_06 = 23
+sol_07 = 21
+sol_08 = 19
+sol_09 = 15
+sol_10 = 13
+sol_11 = 11
+sol_12 = 7
 
 # set pins as output
 GPIO.setup(sol_01, GPIO.OUT, initial=GPIO.LOW)
@@ -45,20 +45,51 @@ def cycle_pin(pin, high_time):
     sleep(high_time)
     print("PIN OFF: {}".format(pin))
     GPIO.output(pin, GPIO.LOW)
+
+def translate_notes(pin):
+    out = sol_01
+    pin = int(pin)
+
+    if (pin == 1):
+        out = sol_01
+    if (pin == 2):
+        out = sol_02
+    if (pin == 3):
+        out = sol_03
+    if (pin == 4):
+        out = sol_04
+    if (pin == 5):
+        out = sol_05
+    if (pin == 6):
+        out = sol_06
+    if (pin == 7):
+        out = sol_07
+    if (pin == 8):
+        out = sol_08
+    if (pin == 9):
+        out = sol_09
+    if (pin == 10):
+        out = sol_10
+    if (pin == 11):
+        out = sol_11
+    if (pin == 12):
+        out = sol_12
+
+    return out
     
 def noteon(pin):
+    pin = translate_notes(pin)
     print("PIN ON: {}".format(pin))
-    #GPIO.output(pin, GPIO.HIGH)
-    pass
+    GPIO.output(pin, GPIO.HIGH)
 
 def noteoff(pin):
+    pin = translate_notes(pin)
     print("PIN OFF: {}".format(pin))
-    #GPIO.output(pin, GPIO.LOW)
-    pass    
+    GPIO.output(pin, GPIO.LOW)
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--ip", default="192.168.1.104", help="The ip to listen on")
+    parser.add_argument("--ip", default="192.168.0.192", help="The ip to listen on")
     parser.add_argument("--port", type=int, default=6666, help="The port to listen on")
     args = parser.parse_args()
   
@@ -88,7 +119,7 @@ if __name__ == "__main__":
 
     # Periodically call osc4py3 processing method in your event loop.
     finished = False
-    cycle = False
+    cycle = True
     while not finished:
         osc_process()
         if cycle:
