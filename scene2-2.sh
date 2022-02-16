77c3bdfea88b7492fa2f3e9268eaf538321a5c43
Samuel Michalik <samuelmichalik@yahoo.com>
2022-02-16 14:11:49 +0100

    improve scene 2 scripts

--- scene2-2.sh
+++ scene2-2.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+export OKC_DISPLAY_HOST="127.0.0.1"
+export OKC_DISPLAY_PORT=5000
+
+export OKC_SPEECH_LANG="en-US"
+export OKC_OUTPUT_SPEECH_LANG="cs-CZ"
+export OKC_ENGINE="davinci"
+
 export OKC_TRANSLATE_FROM_MAIN=
 export OKC_MAIN_HAS_FULLSCREEN=
 export OKC_TRANSLATION_FULLSCREEN=
--- scene2-3.sh
+++ scene2-3.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+export OKC_DISPLAY_HOST="127.0.0.1"
+
+export OKC_SPEECH_LANG="en-US"
+export OKC_OUTPUT_SPEECH_LANG="cs-CZ"
+export OKC_ENGINE="davinci"
+
 export OKC_TRANSLATE_FROM_MAIN=
 export OKC_MAIN_HAS_FULLSCREEN=yes
 export OKC_TRANSLATION_FULLSCREEN=yes
