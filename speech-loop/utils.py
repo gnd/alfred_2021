26656212d93c8a3899ebb8cc8082e5eaecde2bbc
Samuel Michalik <samuelmichalik@yahoo.com>
2022-10-06 22:26:48 +0200

    change display price currency to CZK

--- speech-loop/utils.py
+++ speech-loop/utils.py
@@ -54,7 +54,7 @@ def elapsed_time(start, end):
 def text_to_crowns(text):
     """Apply GPT-3 pricing on the text and return the approximate price in Czech crowns."""
     tokens = len(text) / CHARS_PER_TOK
-    return tokens * USD_PER_1000_TOKS / 1000 * CZK_PER_USD
+    return round(tokens * USD_PER_1000_TOKS / 1000 * CZK_PER_USD, 4)
 
 def text_to_eur(text):
     """Apply GPT-3 pricing on the text and return the approximate price in euros crowns."""
@@ -62,7 +62,8 @@ def text_to_eur(text):
     return round(tokens * USD_PER_1000_TOKS / 1000 * EUR_PER_USD, 4)
 
 def text_coda(text):
-    return f'{text_to_eur(text)} EUR'
+    # return f'{text_to_eur(text)} EUR'
+    return f'{text_to_crowns(text)} CZK'
 
 def recognize_stop_word(text):
     if re.search(r"\b(exit)\b", text, re.I):
